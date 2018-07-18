88f63df27180bb8ef487c6c0cd2080eea537d11f
Etsune <akiyama.myao@gmail.com>
2018-07-18 06:26:31 +0300

    CD upd

--- scripts/collect_data.rb
+++ scripts/collect_data.rb
@@ -27,7 +27,7 @@ genres.each do |gid, _|
             #ranobe_url = "http://ncode.syosetu.com/n3244bb/"
             ranobe_page = Net::HTTP.get(URI.parse(ranobe_url)).force_encoding('UTF-8')
             chapter_text = ranobe_page.scan(chapter_rx)
-            chapter_text = chapter_text || clean_page(chapter_text)
+            chapter_text = clean_page(chapter_text) unless chapter_text.nil?
             if chapter_text.length > 10
                 File.write(data_dir+"/#{gid}_#{rid}_1.txt", chapter_text)
                 next
@@ -37,11 +37,11 @@ genres.each do |gid, _|
                 chapter_url = "http://ncode.syosetu.com" + ch_url
                 chapter_page = Net::HTTP.get(URI.parse(chapter_url)).force_encoding('UTF-8')
                 chapter_text = chapter_page.scan(chapter_rx)
+                next if chapter_text.nil?
                 chapter_text = clean_page(chapter_text)
                 if chapter_text.length > 10
                     #p data_dir+"/#{gid}_#{rid}_#{cn}.txt"
                     File.write(data_dir+"/#{gid}_#{rid}_#{cn}.txt", chapter_text)
-                    next
                 end
             end
         end
