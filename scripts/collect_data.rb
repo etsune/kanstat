13b04667fe542f4b2cb490159fe4cef3eb158e38
Etsune <akiyama.myao@gmail.com>
2018-07-20 06:44:29 +0300

    dbg fix

--- scripts/collect_data.rb
+++ scripts/collect_data.rb
@@ -37,29 +37,35 @@ loop do
 
         #считает количество скачанных тайтлов для данного жанра
         getted = 0
+        puts("Selected genre #{gid}")
         
         #перебор страниц поиска
         (1..100).each do |page|
 
-            #после каждых 10 файлов переходить к следующему жанру
+            #после каждых 10 тайтлов переходить к следующему жанру
             #задержка 1 сек для избежания блокировки (она есть, проверено)
-            break if getted == 10
+            break if getted > 10
             sleep(1)
             search_url = "http://yomou.syosetu.com/search.php?&order=notorder&notnizi=1&genre=#{gid}&p=#{page}"
             search_page = Net::HTTP.get(URI.parse(search_url)).force_encoding('UTF-8')
             search_page.scan(search_rx).each do |rid, name|
+                
+                #debug
+                getted += 1
+                puts()
+
                 if is_id_saved(rid, data_dir)
                     puts "Skip #{rid}"
                     next
                 end
-                getted += 1
+                #getted += 1
                 ranobe_url = "http://ncode.syosetu.com/#{rid}/"
                 puts("Reading " + rid)
                 #ranobe_url = "http://ncode.syosetu.com/n3244bb/"
                 ranobe_page = Net::HTTP.get(URI.parse(ranobe_url)).force_encoding('UTF-8')
                 chapter_text = ranobe_page.scan(chapter_rx)
 
-                #сохраняем ранобе из 1 главы, или идём дальше, если глав больше
+                #сохраняем ранобе из 1 главы или идём дальше, если глав больше
                 if chapter_text.to_s.length > 10
                     File.write(data_dir+"/#{gid}_#{rid}_1.txt", clean_page(chapter_text))
                     next
@@ -67,7 +73,7 @@ loop do
 
                 ranobe_page.scan(chapter_urls_rx) do |ch_url, cn|
                     sleep(1)
-                    puts("Ch " + cn)
+                    print(cn+"-")
                     chapter_url = "http://ncode.syosetu.com" + ch_url
                     chapter_page = Net::HTTP.get(URI.parse(chapter_url)).force_encoding('UTF-8')
                     chapter_text = chapter_page.scan(chapter_rx)
