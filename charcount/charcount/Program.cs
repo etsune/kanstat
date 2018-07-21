fa773f7f66de2a5c015dd819ef0bdb9584f10fb7
Etsune <akiyama.myao@gmail.com>
2018-07-21 15:32:21 +0300

    Add order

--- charcount/charcount/Program.cs
+++ charcount/charcount/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace charcount
@@ -11,6 +12,7 @@ namespace charcount
         static string data_dir, result_dir;
         static Regex ch_name_rx = new Regex(@"(?<g>\d+)_(?<id>\w+)_(?<n>\d+)\.txt");
         static Dictionary<string, Dictionary<string, int>> result = new Dictionary<string, Dictionary<string, int>>();
+        static Dictionary<string, int> total_count = new Dictionary<string, int>();
 
         static void Main(string[] args)
         {
@@ -21,6 +23,8 @@ namespace charcount
                 Match ch_name_mc = ch_name_rx.Match(file.Name);
                 string chapter_id = ch_name_mc.Groups["id"].Value;
                 string chapter_genre = ch_name_mc.Groups["g"].Value;
+                int g_total = 0;
+
                 string chapter = File.ReadAllText(file.FullName);
                 foreach(char c in chapter)
                 {
@@ -38,14 +42,47 @@ namespace charcount
 
                     result[s][chapter_genre]++;
                     result[s]["0"]++;
+                    g_total++;
                 }
+                if (!total_count.ContainsKey(chapter_genre))
+                    total_count.Add(chapter_genre, 0);
+                total_count[chapter_genre] += g_total;
+                total_count["0"] += g_total;
             }
 
+            result_reformat();
             string json = JsonConvert.SerializeObject(result, Formatting.Indented);
 
             string result_file = result_dir + @"\stats.json";
             if (File.Exists(result_file)) File.Delete(result_file);
             File.WriteAllText(result_file, json);
+        }
+
+        static void result_reformat()
+        {
+            foreach(var g_count in total_count)
+            {
+                var recounter = new Dictionary<string, int>();
+                foreach (var glypf in result)
+                {
+                    if (!glypf.Value.ContainsKey(g_count.Key))
+                    {
+                        result[glypf.Key].Add(g_count.Key, 0);
+                        recounter.Add(glypf.Key, 0);
+                    }
+                    else
+                    {
+                        recounter.Add(glypf.Key, glypf.Value[g_count.Key]);
+                    }
+                }
+                var sortedDict = from entry in recounter orderby entry.Value descending select entry;
+                int sd_count = 0;
+                foreach (var sd in sortedDict)
+                {
+                    sd_count++;
+                    result[sd.Key]["n" + g_count.Key] = sd_count;
+                }
+            }
         }
 
         static void init_env()
@@ -62,6 +99,7 @@ namespace charcount
             result_dir = Console.ReadLine();
             if (result_dir.Length < 3)
                 result_dir = default_result_dir;
+            total_count.Add("0", 0);
         }
     }
 }
