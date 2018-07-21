ef210cc6d9dc1447cd016cdf0ec37499bd66ba2f
Etsune <akiyama.myao@gmail.com>
2018-07-21 08:15:02 +0300

    working with tmp

--- scripts/collect_data.rb
+++ scripts/collect_data.rb
@@ -8,48 +8,80 @@ search_old_rx = /(?<url>https?:\/\/ncode\.syosetu\.com\/(?<rid>[\w]+)\/)"/
 search_rx = /href="https:\/\/ncode\.syosetu\.com\/(?<rid>.+?)\/">(?<name>.+?)<\/a>[\s\S]+?<table>/
 chapter_urls_rx = /<a href="(?<ch>\/.+?\/(?<cn>\d*)\/)">.+?<\/a>/
 
-#директория для скачивания ранобэ
-data_dir = File.expand_path("../../data/", __FILE__)
+# Директория для скачивания ранобэ
+$data_dir = File.expand_path("../../data/", __FILE__)
 
-#получаем список жанров
-genres = JSON.parse(File.read(File.expand_path("../../result/", __FILE__)+'/genres.json'))
+$tmp_dir = File.expand_path("../../tmp/", __FILE__)
 
-#удалить все сторонние теги из главы
+# Получаем список жанров
+$genres = JSON.parse(File.read(File.expand_path("../../result/", __FILE__) + '/genres.json'))
+
+$data_list_file = $tmp_dir + '/data_list.txt'
+
+# Удалить все сторонние теги из главы
 def clean_page (text)
     chapter_tags_rx = /<\/?p.*?>/ 
     br_rx = /<br.+?>/
     text[0][0].gsub(chapter_tags_rx,"").gsub(br_rx,"")
 end
 
-#проверяем, сохраняли ли это ранобэ ранее
-def is_id_saved (id, data_dir)
-    Dir[data_dir+"/*"].each do |file|
+# Проверяем, сохраняли ли это ранобэ ранее
+def is_id_saved (id)
+    Dir[$data_dir+"/*"].each do |file|
         return true if file.include?(id)
+    end
+    begin
+        file = File.new($data_list_file, "r")
+        while (line = file.gets)
+            if line.include?(id)
+                file.close
+                return true
+            end
+        end
+        file.close
+    rescue => err
+        puts "Exception: #{err}"
+        err
     end
     return false
 end
 
-#бесконечное сканирование
+# Составить список скачанных глав
+def generate_data_list ()
+    open($data_list_file, 'a') do |f|
+        Dir[$data_dir+"/*"].each do |file|
+            f.puts file.match(/(?<f>[\w]+?)\.txt\z/i)["f"]
+        end
+    end
+end
+
+# Начало программы
+Dir.mkdir($data_dir) unless Dir.exist?($data_dir)
+Dir.mkdir($tmp_dir) unless Dir.exist?($tmp_dir)
+File.delete($data_list_file) if File.exist?($data_list_file)
+generate_data_list()
+
+# Бесконечное сканирование
 loop do
 
-    #поиск по каждому жанру по очереди
-    genres.each do |gid, _|
+    # Поиск по каждому жанру по очереди
+    $genres.each do |gid, _|
 
-        #считает количество скачанных тайтлов для данного жанра
+        # Считает количество скачанных тайтлов для данного жанра
         getted = 0
         puts("Selected genre #{gid}")
         
-        #перебор страниц поиска
+        # Перебор страниц поиска
         (1..100).each do |page|
 
-            #после каждых 10 тайтлов переходить к следующему жанру
-            #задержка 1 сек для избежания блокировки (она есть, проверено)
+            # После каждых 10 тайтлов переходить к следующему жанру
+            # Задержка 1 сек для избежания блокировки (она есть, проверено)
             break if getted > 10
             sleep(1)
             search_url = "http://yomou.syosetu.com/search.php?&order=notorder&notnizi=1&genre=#{gid}&p=#{page}"
             search_page = Net::HTTP.get(URI.parse(search_url)).force_encoding('UTF-8')
             search_page.scan(search_rx).each do |rid, name|
-                if is_id_saved(rid, data_dir)
+                if is_id_saved(rid)
                     puts "Skip #{rid}"
                     next
                 end
@@ -61,9 +93,9 @@ loop do
                 ranobe_page = Net::HTTP.get(URI.parse(ranobe_url)).force_encoding('UTF-8')
                 chapter_text = ranobe_page.scan(chapter_rx)
 
-                #сохраняем ранобе из 1 главы или идём дальше, если глав больше
+                # Сохраняем ранобе из 1 главы или идём дальше, если глав больше
                 if chapter_text.to_s.length > 10
-                    File.write(data_dir+"/#{gid}_#{rid}_1.txt", clean_page(chapter_text))
+                    File.write($data_dir+"/#{gid}_#{rid}_1.txt", clean_page(chapter_text))
                     print("1")
                     next
                 end
@@ -76,7 +108,12 @@ loop do
                     chapter_text = chapter_page.scan(chapter_rx)
 
                     if chapter_text.to_s.length > 10
-                        File.write(data_dir+"/#{gid}_#{rid}_#{cn}.txt", clean_page(chapter_text))
+                        saved_file_name = "#{gid}_#{rid}_#{cn}"
+                        File.write($data_dir+"/#{saved_file_name}.txt", clean_page(chapter_text))
+
+                        open($data_list_file, 'a') do |f|
+                            f.puts saved_file_name
+                        end
                     end
                 end
                 puts()
