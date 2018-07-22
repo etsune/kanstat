facf095b81c9f8820fcf9e1df28ae63b5b9cc9f6
Etsune <akiyama.myao@gmail.com>
2018-07-22 06:14:34 +0300

    tables

---
+++ frontend/js/main.js
@@ -0,0 +1,69 @@
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        kanstat_data_url: "https://raw.githubusercontent.com/etsune/kanstat/master/result/stats.min.json",
+        genres_data_url: "https://raw.githubusercontent.com/etsune/kanstat/master/result/genres.json",
+        genres_data: {},
+        kanstat_data: {},
+        active_stat: 0,
+        aobj: [],
+    },
+    methods: {
+        loading: function () {
+            this.$http.get(this.kanstat_data_url).then(function (response) {
+                this.$http.get(this.genres_data_url).then(function (response2) {
+                    this.kanstat_data = response.data;
+                    this.genres_data = response2.data;
+                    this.active_obj_create(this.active_stat);
+                }, function (error2) {
+                });
+            }, function (error) {
+            });
+        },
+        is_active: function (gid) {
+            return { 'active': gid == this.active_stat }
+        },
+        go_to: function (gid) {
+            this.active_stat = gid
+            //this.active_obj_create(gid);
+        },
+        active_obj_create: function (gid) {
+            for (var property in this.kanstat_data) {
+                if (this.kanstat_data.hasOwnProperty(property)) {
+                    var j = this.kanstat_data[property];
+                    var used = j[gid];
+                    var glyph = property;
+                    var rank = j["n"+gid];
+                    var freq = j["f"+gid];
+                    this.aobj[rank] = { 
+                        glyph: glyph,
+                        freq: freq,
+                        used: used,
+                    };
+                }
+            }
+        }
+        
+    },
+    created: function () {
+        this.loading();
+    },
+    components: {
+        test: {
+            template: '#ad-nav',
+            data: function () {
+                return {
+                    searching: testjson,
+                }
+            },
+            props: {
+                nviewer: 0,
+            },
+        },
+        kantable: {
+            template: '#kantable',
+            props: ['activestat','aeobj'],
+        }
+    }
+})
