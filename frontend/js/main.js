47d3e7b5c8a258d7f2a61f0ffa2d51a73c1943f5
Etsune <akiyama.myao@gmail.com>
2018-07-22 08:15:51 +0300

    table rendering

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,6 +8,7 @@ var vm = new Vue({
         kanstat_data: {},
         active_stat: 0,
         aobj: [],
+        loader: true,
     },
     methods: {
         loading: function () {
@@ -16,6 +17,7 @@ var vm = new Vue({
                     this.kanstat_data = response.data;
                     this.genres_data = response2.data;
                     this.active_obj_create(this.active_stat);
+                    this.loader = false;
                 }, function (error2) {
                 });
             }, function (error) {
@@ -29,18 +31,27 @@ var vm = new Vue({
             this.active_obj_create(gid);
         },
         active_obj_create: function (gid) {
+            this.loader = true;
+            this.aobj = [];
             for (var property in this.kanstat_data) {
                 if (this.kanstat_data.hasOwnProperty(property)) {
-                    vm.$set(this.aobj, this.kanstat_data[property]["n"+gid], { 
-                        glyph: property,
-                        freq: this.kanstat_data[property]["f"+gid],
-                        used: this.kanstat_data[property][gid],
-                    });
+                    if (!this.kanstat_data[property][gid]) {
+                        // vm.$set(this.aobj, 0, {
+                        //     glyph: "",
+                        //     freq: "",
+                        //     used: "",
+                        // });
+                    } else {
+                        vm.$set(this.aobj, this.kanstat_data[property]["n" + gid] - 1, {
+                            glyph: property,
+                            freq: this.kanstat_data[property]["f" + gid],
+                            used: this.kanstat_data[property][gid],
+                        });
+                    }
                 }
             }
-            console.log(this.aobj);
+            this.loader = false;
         }
-        
     },
     created: function () {
         this.loading();
@@ -59,7 +70,18 @@ var vm = new Vue({
         },
         kantable: {
             template: '#kantable',
-            props: ['activestat','aeobj'],
+            props: ['activestat', 'aeobj'],
+            methods: {
+                byars_link: function (kanji) {
+                    return "http://e-lib.ua/dic/results?w=" + kanji + "&m=0";
+                },
+                jisho_link: function (kanji) {
+                    return "https://jisho.org/search/" + kanji + "%20%23kanji";
+                },
+                checkdata: function () {
+                    return this.aeobj.length > 0;
+                }
+            }
         }
     }
 })
